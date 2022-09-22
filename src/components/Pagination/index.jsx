ad70b9cde9a3657928c181032aeca73d072bea72
xvishnevskix <45593711+xvishnevskix@users.noreply.github.com>
2022-09-22 15:54:08 +0400

    Pagination Implemented

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,8 +1,18 @@
+import ReactPaginate from "react-paginate";
+import styles from "./Pagination.module.scss"
 
-
-const Pagination = () => {
+const Pagination = ({onChangePage}) => {
     return (
-        <></>
+        <ReactPaginate
+            className={styles.root}
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={(event) => onChangePage(event.selected + 1)}
+            pageRangeDisplayed={8}
+            pageCount={2}
+            previousLabel="<"
+            renderOnZeroPageCount={null}
+        />
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
-import ReactPaginate from "react-paginate";
+import Pagination from "../components/Pagination";
+
+
 
  const Home = ({searchValue}) => {
 
@@ -11,6 +13,7 @@ import ReactPaginate from "react-paginate";
     const [isLoading, setIsLoading] = React.useState(true)
      const [selectedSort, setSelectedSort] = React.useState({name: 'популярности', sortType: 'rating'})
      const [categoryId, setCategoryId] = React.useState(0)
+     const [currentPage, setCurrentPage] = React.useState(1)
 
 
 
@@ -21,7 +24,7 @@ import ReactPaginate from "react-paginate";
         const sortBy = selectedSort.sortType.replace('-','')
         const search = searchValue ? `&search=${searchValue}` : ''
 
-        fetch(`https://6311b8dd19eb631f9d779584.mockapi.io/items?sortBy=${sortBy}&order=${sortMethod}&category=${categoryId}`)
+        fetch(`https://6311b8dd19eb631f9d779584.mockapi.io/items?page=${currentPage}&limit=8&sortBy=${sortBy}&order=${sortMethod}&category=${categoryId}`)
 
             .then((res) => res.json())
             .then((arr) => {
@@ -29,7 +32,7 @@ import ReactPaginate from "react-paginate";
                 setIsLoading(false)
             })
         window.scrollTo(0,0)
-    }, [categoryId, selectedSort, searchValue])
+    }, [categoryId, selectedSort, searchValue,currentPage])
 
      const skeleton = [...new Array(9)].map((_, index) => <Skeleton key={index}/>)
 
@@ -50,15 +53,7 @@ import ReactPaginate from "react-paginate";
     <div className="content__items">
         {isLoading ? skeleton : pizzas}
     </div>
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="next >"
-                onPageChange={(event) => console.log(event)}
-                pageRangeDisplayed={8}
-                pageCount={3}
-                previousLabel="< previous"
-                renderOnZeroPageCount={null}
-            />
+            <Pagination onChangePage={(id) => setCurrentPage(id)}/>
         </>
     )
 }
